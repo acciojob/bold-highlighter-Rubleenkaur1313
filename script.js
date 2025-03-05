9328e77748f03270ae711f62708fd8c10bc3eb32
Rubleenkaur1313 <rubleenkaur05@gmail.com>
2025-03-05 15:54:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function highlight() {
-    //Write your code here
-
-
+    // Select all <strong> elements
+    const boldElements = document.querySelectorAll('strong');
+    
+    // Loop through each <strong> element and change its color to green
+    boldElements.forEach(function(element) {
+        element.style.color = 'rgb(0, 128, 0)';
+    });
 }
 
-
 function return_normal() {
-    //Write your code here
-
+    // Select all <strong> elements
+    const boldElements = document.querySelectorAll('strong');
     
+    // Loop through each <strong> element and revert its color to black
+    boldElements.forEach(function(element) {
+        element.style.color = 'rgb(0, 0, 0)';
+    });
 }
